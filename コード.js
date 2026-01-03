abe6ae7fae6d1cc4f9e661ebd79433d40fb3c381
TakayukiHirano117 <ikusa.hirano@icloud.com>
2026-01-03 18:59:51 +0900

    LINE通知を追加

--- コード.js
+++ コード.js
@@ -1,5 +1,23 @@
-function sendEmailFromGoogleFormContent(e) {
+function main(e) {
+  const { name, email, schedule } = getFormResponses(e);
+
+  sendEmail(name, email, schedule);
+  notifyLINE(name, email, schedule);
+}
+
+/**
+ * Googleフォーム送信時に実行される
+ * @param {GoogleAppsScript.Events.FormsOnFormSubmit} e
+ */
+const getFormResponses = (e) => {
+  if (!e) {
+    throw new Error('イベントが発火していません')
+  }
+
   const responses = e.response.getItemResponses();
+  if (!responses) {
+    throw new Error('レスポンスが存在しません')
+  }
 
   const responseObj = {}
   responses.forEach((response) => {
@@ -7,11 +25,40 @@ function sendEmailFromGoogleFormContent(e) {
   })
 
   const formItemTitleMapping = getFormItemTitleMapping();
-
   const name = responseObj[formItemTitleMapping['name']];
   const email = responseObj[formItemTitleMapping['email']];
   const schedule = responseObj[formItemTitleMapping['schedule']];
 
+  return {
+    name: name,
+    email: email,
+    schedule: schedule
+  }
+}
+
+const buildMailBody = (name, schedule) => {
+  return `
+    ${name} 様
+
+    この度はお申込みいただきありがとうございます。
+
+    以下の日程でお申し込みを受け付けました。
+
+    ${schedule}
+  `;
+}
+
+const buildLineMessageBody = (name, email, schedule) => {
+  return `
+    下記日程に申し込みが入りました。 
+
+    お名前: ${name}
+    メールアドレス: ${email}
+    日程: ${schedule}
+  `;
+}
+
+const sendEmail = (name, email, schedule) => {
   const subject = "イベント申し込み確認メール"
   const body = buildMailBody(name, schedule);
   const options = {
@@ -19,18 +66,6 @@ function sendEmailFromGoogleFormContent(e) {
   }
 
   GmailApp.sendEmail(email, subject, body, options)
-}
-
-const buildMailBody = (name, schedule) => {
-  return `
-${name} 様
-
-この度はお申込みいただきありがとうございます。
-
-以下の日程でお申し込みを受け付けました。
-
-${schedule}
-  `;
 }
 
 const getFormItemTitleMapping = () => {
@@ -41,3 +76,31 @@ const getFormItemTitleMapping = () => {
   }
 }
 
+function notifyLINE(name, email, schedule) {
+  const props = PropertiesService.getScriptProperties();
+
+  const headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Authorization': `Bearer ${props.getProperty('CHANNEL_ACCESS_TOKEN')}`,
+  }
+
+  const message = buildLineMessageBody(name, email, schedule);
+
+  const data = {
+    'to': props.getProperty('USER_ID'),
+    'messages': [
+      {
+        'type': 'text',
+        'text': message 
+      }
+    ],
+  }
+
+  const options = {
+    'method': 'post',
+    'headers': headers,
+    'payload': JSON.stringify(data),
+  }
+
+  UrlFetchApp.fetch(props.getProperty('PUSH_MESSAGE_URL'), options);
+}
